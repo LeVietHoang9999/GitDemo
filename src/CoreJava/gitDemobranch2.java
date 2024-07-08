2c94db33ce785424f34e857e2038fd22a86d6908
LeVietHoang9999 <tester.hoangleviet1999@gmail.com>
2024-07-08 09:55:54 +0700

    This is the commit from Thailand Development 2nd try

---
+++ src/CoreJava/gitDemobranch2.java
@@ -0,0 +1,9 @@
+package CoreJava;
+
+public class gitDemobranch2 {
+	public void getData() {
+		System.out.println("This is the Git Demo method1");
+		System.out.println("This is the Git Demo method2");
+		System.out.println("This is the Git Demo method3");
+	}
+}
